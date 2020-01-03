1136f55b77ca877a42acbd5e008b7086f7bf9f0c
jaewookie <kjoop@naver.com>
2020-01-03 12:22:43 +0900

    mongo db connection initializing

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
+var password = process.env.PASSWORD
+mongoose.connect(`mongodb+srv://root:${password}@jaewook-0sogy.mongodb.net/test?retryWrites=true&w=majority`,{ useNewUrlParser: true , useUnifiedTopology: true })
+// ('mongodb+srv://root:1234@jaewook-0sogy.mongodb.net/test?retryWrites=true&w=majority')
+
+var db = mongoose.connection
+
+if (!db) {
+    console.log('err')
+}else{
+    console.log('con')
+}
+
 var port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
