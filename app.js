ce04f0f44e3673e5ee92643f57f51a0426e3106e
jaewookie <kjoop@naver.com>
2020-01-03 12:02:56 +0900

    add mongoose labrary

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var app = express()
 var path = require('path')
 var ejs = require('ejs')
 require('dotenv').config()
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 var port = process.env.PORT || 3000
 
